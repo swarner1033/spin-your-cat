579826356990b7323c868408bcfb610e0308df7a
Vanessa Luvio <vanessa.luvio@hotmail.com>
2024-09-08 11:36:42 +1000

    Font Changed

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Spin that cat</h1>
+      <h1>Spin that cat so fasttttttt</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
