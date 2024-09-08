887f30dacb96abd62bf387ab630c6e3f8b2b4453
Vanessa Luvio <vanessa.luvio@hotmail.com>
2024-09-08 12:17:02 +1000

    Circle button

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Spin that cat so fasttttttt</h1>
+      <button className='circle' >Spin that cat</button>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
